7d3d03d9b7cb44c0957f189005411b4b39985168
komei-work-work <koba.work,work1127@gmail.com>
2019-09-17 00:44:57 +0900

    [first] loggers

---
+++ src/interface.ts
@@ -0,0 +1,33 @@
+export enum LogLevel {
+    Error = 'error',
+    Warn = 'warn',
+    Info = 'info',
+    Debug = 'debug',
+}
+
+export interface Logger<C = string, O = {}> {
+    error(content: C, ...optional: O[]): void;
+    warn(content: C, ...optional: O[]): void;
+    info(content: C, ...optional: O[]): void;
+    debug(content: C, ...optional: O[]): void;
+}
+export namespace Logger {
+    export function log<C = string, O = {}>(logger: Logger<C, O>, level: LogLevel, content: C, optional: O[]): void {
+        logger[level].call(logger, content, ...optional);
+    }
+}
+
+export interface GenericLogger<L> {
+    log(level: LogLevel, log: L): void;
+}
+
+export interface Log<C = string, O = {}> {
+    content: C;
+    optional: O[];
+}
+
+export interface TaggedLog<C = string, O = {}> extends Log<C, O> {
+    tags: string[];
+}
+
+export type LogRewrite<C = string, O = {}> = (log: Log<C, O>) => void;
