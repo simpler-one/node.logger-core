502ede65c1ea8f6ad2048439aaa83bb5f3b68bcc
komei-work-work <koba.work,work1127@gmail.com>
2019-09-23 11:42:39 +0900

    [first] skip buffer

--- src/log.stream/extensions/skip.ts
+++ src/log.stream/extensions/skip.ts
@@ -1,14 +1,19 @@
 import { LogInputStream, Log } from '../../interface';
 import { LogStream } from '../log.stream';
 
-type Filter<T> = (log: Log<T>) => boolean;
+type Filter<T> = (log: Log<T>, histories: Log<T>[]) => boolean;
 interface SkipFinishedParam {
     skipCount: number;
 }
 
 declare module '../log.stream' {
     interface LogStream<T> {
-        skip(filter: Filter<T>, onStartSkip: () => Log<T>, onFinishSkip: (params: SkipFinishedParam) => Log<T>): LogStream<T>;
+        skip(
+            filter: Filter<T>,
+            bufferSize?: number,
+            onStartSkip?: () => Log<T>,
+            onFinishSkip?: (params: SkipFinishedParam) => Log<T>,
+        ): LogStream<T>;
     }
 }
 
@@ -19,19 +24,23 @@ class SkippedLogStream<T> implements LogInputStream<T> {
         return this.skipCount > 0;
     }
 
+    private readonly histories: Log<T>[] = [];
+
     constructor(
         private readonly stream: LogInputStream<T>,
         private readonly filter: Filter<T>,
+        private readonly bufferSize: number,
         private readonly onStartSkip?: () => Log<T>,
         private readonly onFinishSkip?: (params: SkipFinishedParam) => Log<T>
     ) {
     }
 
     public write(log: Log<T>): void {
-        if (this.filter(log)) {
+        if (this.filter(log, [...this.histories])) {
             if (!this.skipping && this.onStartSkip) {
                 this.stream.write(this.onStartSkip());
             }
+
             this.skipCount++;
         } else {
             if (this.skipping && this.onFinishSkip) {
@@ -41,10 +50,20 @@ class SkippedLogStream<T> implements LogInputStream<T> {
             this.stream.write(log);
             this.skipCount = 0;
         }
+
+        this.histories.push(log);
+        if (this.histories.length > this.bufferSize) {
+            this.histories.shift();
+        }
     }
 }
 
 
-LogStream.prototype.skip = function<T>(filter: Filter<T>): LogStream<T> {
-    return new LogStream(new SkippedLogStream(this.stream, filter));
+LogStream.prototype.skip = function<T>(
+    filter: Filter<T>,
+    bufferSize?: number,
+    onStartSkip?: () => Log<T>,
+    onFinishSkip?: (params: SkipFinishedParam) => Log<T>,
+): LogStream<T> {
+    return new LogStream(new SkippedLogStream(this.stream, filter, bufferSize, onStartSkip, onFinishSkip));
 }
