ec635565279680039cdd65a6334bf2f3549176e9
work-work-komei <40133262+work-work-komei@users.noreply.github.com>
2019-10-01 08:42:12 +0900

    Update logger-stream.adapter.ts

--- logger-stream.adapter.ts
+++ logger-stream.adapter.ts
@@ -6,7 +6,7 @@ export class LoggerStreamAdapter<C, O> implements LogInputStream<LogBody<C, O>> 
     ) {
     }
 
-    public static asFullLevel<C, O>(logger: FullLevelLogger<C, O>): LogStreamAdapter<C, O> {
+    public static asFull<C, O>(logger: FullLevelLogger<C, O>): LogStreamAdapter<C, O> {
         return new LogStreamAdapter(new Map([
             [LogLevel.Fatal, logger.fatal.bind(logger)],
             [LogLevel.Error, logger.error.bind(logger)],
@@ -14,6 +14,26 @@ export class LoggerStreamAdapter<C, O> implements LogInputStream<LogBody<C, O>> 
             [LogLevel.Info, logger.info.bind(logger)],
             [LogLevel.Debug, logger.debug.bind(logger)],
             [LogLevel.Trace, logger.trace.bind(logger)],
+        ]));
+    }
+    public static asNormal<C, O>(logger: FullLevelLogger<C, O>): LogStreamAdapter<C, O> {
+        return new LogStreamAdapter(new Map([
+            [LogLevel.Fatal, logger.error.bind(logger)],
+            [LogLevel.Error, logger.error.bind(logger)],
+            [LogLevel.Warn, logger.warn.bind(logger)],
+            [LogLevel.Info, logger.info.bind(logger)],
+            [LogLevel.Debug, logger.debug.bind(logger)],
+            [LogLevel.Trace, logger.debug.bind(logger)],
+        ]));
+    }
+    public static asMinimum<C, O>(logger: FullLevelLogger<C, O>): LogStreamAdapter<C, O> {
+        return new LogStreamAdapter(new Map([
+            [LogLevel.Fatal, logger.error.bind(logger)],
+            [LogLevel.Error, logger.error.bind(logger)],
+            [LogLevel.Warn, logger.error.bind(logger)],
+            [LogLevel.Info, logger.info.bind(logger)],
+            [LogLevel.Debug, logger.info.bind(logger)],
+            [LogLevel.Trace, logger.info.bind(logger)],
         ]));
     }
 
