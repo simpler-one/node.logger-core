8cfc4a6180e70308ea82ff94f0074ff2b77431c2
work-work-komei <40133262+work-work-komei@users.noreply.github.com>
2019-10-01 08:22:27 +0900

    Update logger-stream.adapter.ts

--- logger-stream.adapter.ts
+++ logger-stream.adapter.ts
@@ -9,7 +9,13 @@ export class LoggerStreamAdapter<C, O> implements LogInputStream<LogBody<C, O>> 
     public static asFullLevel<C, O>(logger: FullLevelLogger<C, O>): LogStreamAdapter<C, O> {
         return new LogStreamAdapter(new Map([
             [LogLevel.Fatal, logger.fatal.bind(logger)],
+            [LogLevel.Error, logger.error.bind(logger)],
+            [LogLevel.Warn, logger.warn.bind(logger)],
+            [LogLevel.Info, logger.info.bind(logger)],
+
         ]));
     }
+
+
 }
   
